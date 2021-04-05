873c88eebe088bcdca9e92db78b7d4fed367c3dc
mfmitsch <mfmitsch@iu.edu>
2021-04-05 14:54:25 -0400

    round numbers

--- calculator.js
+++ calculator.js
@@ -18,25 +18,25 @@ function update(){
     section_2_1_input = parseInt(document.getElementById("section 2.1 in").value);
     section_2_2_input = parseInt(document.getElementById("section 2.2 in").value);
     section_2_3_input = parseInt(document.getElementById("section 2.3 in").value);
-    if (isNaN(section_1_input)){
+    if (isNaN(section_1_input || section_1_input > 45 || section_1_input < 0)){
         section_1_out = 0;
     }
     else {
         section_1_out = section_1_input;
     }
-    if (isNaN(section_2_1_input)){
+    if (isNaN(section_2_1_input) || section_2_1_input > 6 || section_2_1_input < 0){
         section_2_1_out = 0;
     }
     else {
         section_2_1_out = section_2_1_input * 3.0555;
     }
-    if (isNaN(section_2_2_input)){
+    if (isNaN(section_2_2_input || section_2_2_input > 6 || section_2_2_input < 0)){
         section_2_2_out = 0;
     }
     else {
         section_2_2_out = section_2_2_input * 3.0555;
     }
-    if (isNaN(section_2_3_input)) {
+    if (isNaN(section_2_3_input || section_2_3_input > 6 || section_2_3_input < 0)) {
         section_2_3_out = 0;
     }
     else {
@@ -62,12 +62,12 @@ function update(){
         score = 5;
     }
     document.getElementById("section 1 out").value = "" + section_1_out;
-    document.getElementById("section 2.1 out").value = "" + section_2_1_out;
-    document.getElementById("section 2.2 out").value = "" + section_2_2_out;
-    document.getElementById("section 2.3 out").value = "" + section_2_3_out;
-    document.getElementById("sum").value = "" + sum;
+    document.getElementById("section 2.1 out").value = "" + section_2_1_out.toFixed(4);
+    document.getElementById("section 2.2 out").value = "" + section_2_2_out.toFixed(4);
+    document.getElementById("section 2.3 out").value = "" + section_2_3_out.toFixed(4);
+    document.getElementById("sum").value = "" + sum.toFixed(4);
     document.getElementById("weight 1").value = "" + weighted_1;
-    document.getElementById("weight 2").value = "" + weighted_2;
+    document.getElementById("weight 2").value = "" + weighted_2.toFixed(4);
     document.getElementById("composite score").value = "" + composite;
     document.getElementById("1").style.border = "none";
     document.getElementById("2").style.border = "none";
